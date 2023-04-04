6a5c94b9d2abf2949765c67a54305dd1589cc687
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-04-04 15:02:08 +0200

    Creazione Cartelle e scrittura ragionamento prima dello svolgimento dell'esercizio

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/*
+Descrizione:
+    - Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+    - Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    - Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+/* ------------------- Sviluppo Esercizio ------------------ */
+
+// 1.- Inizio col creare la funzione per i 5 numeri random
+
+// 2.- Creo un'array vuoto dove inserire i numeri random
+
+// 3.- Creo una condizione che mi permetta di inserire i numeri random senza che si ripetano:
+//       - Ogni volta che il numero viene inserito nell'array dovrà essere mostrato e stampato anche sul documento
+//       - Al termine dell'inserimento i numeri dovranno scomparire
+//       - Dovrà comparire un input che mi permetterà di inserire i numeri all'interno dell'array:
+//          ° Dopo aver scritto i numeri, una condizione mi dirà se i numeri sono presenti all'interno dell'array

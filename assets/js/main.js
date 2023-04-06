a79e9a1717322ad5ad0e96666137a1feb31e571e
AlvaroArbaiza <122307217+AlvaroArbaiza@users.noreply.github.com>
2023-04-06 14:08:17 +0200

    Refactoring - Funzioni con setTimout corrette

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,8 @@ let num;
 // Variabile che riutilizzerò per ( setTimeout(myCountdown, 3000) )
 let clock1;
 
+let clock2;
+
 // Variabile array vuota
 let array = [];
 
@@ -50,9 +52,13 @@ function playButton(){
             // Invoco la funzione con una variabile per avere lo stesso valore ovunque
             num = numRandom() 
     
-            // Viene pushato ( numRandom() )
-            array.push( num )
+            // Se nell'array non c'è il valore ( num )
+            if (!array.includes(num)) {
             
+                // Viene pushato ( num )
+                array.push(num)
+            }    
+
             // Ogni volta aggiungo +1 a i
             i++
 
@@ -60,7 +66,7 @@ function playButton(){
             ulNumeri.innerHTML +=
             `
                 <li><h3>${i}o Numero:</h3> <h2>${num}</h2></li>
-            `            
+            `     
         }
     
         return array
@@ -69,6 +75,8 @@ function playButton(){
 
     // Invoco la funzione per far sparire la lista dopo 3 secondi
     clock1 = setTimeout(myCountdown, 3000);
+
+    clock2 = setTimeout(myCountdown2, 3500);
 }
 
 // Array vuoto per i prompt
@@ -79,132 +87,15 @@ function myCountdown() {
 
     // Inserisco uno spazio vuoto per resettare il contenuto nel DOM
     ulNumeri.innerHTML = "";
-    
-    // Costante per avere un intervallo di tempo tra il reset del DOM e la comparsa dei PROMPT
-    const myInterval = setInterval(myTimer, 1);
-    
-    // Funzione per avere un intervallo di 1 millisecondo di tempo
-    function myTimer() {
-
-        // Creazione array prompt
-        arrayPrompt[0] = parseInt(prompt("Inserisci un numero di quelli che hai visto"));
-        arrayPrompt[1] = parseInt(prompt("Inserisci un numero"));
-        arrayPrompt[2] = parseInt(prompt("Inserisci un numero")); 
-        arrayPrompt[3] = parseInt(prompt("Inserisci un numero"));
-        arrayPrompt[4] = parseInt(prompt("Inserisci un numero")); 
-        
-        // arrayPrompt[0]
-        if (arrayPrompt[0] == array[0]) {
-            
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[0]}</strong>, è lo stesso del 1o Numero!
-                </li>   
-            `
-        } else  {
-
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[0]}</strong>, è diverso dal 1o Numero!
-                </li>   
-            `
-        }
-
-        // arrayPrompt[1]
-        if (arrayPrompt[1] == array[1]) {
-            
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[1]}</strong>, è lo stesso del 2o Numero!
-                </li>   
-            `
-        } else  {
-
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[1]}</strong>, è diverso dal 2o Numero!
-                </li>   
-            `
-        }
-
-        // arrayPrompt[2]
-        if (arrayPrompt[2] == array[2]) {
-            
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[2]}</strong>, è lo stesso del 3o Numero!
-                </li>   
-            `
-        } else  {
-
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[2]}</strong>, è diverso dal 3o Numero!
-                </li>   
-            `
-        }
-
-        // arrayPrompt[3]
-        if (arrayPrompt[3] == array[3]) {
-            
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[3]}</strong>, è lo stesso del 4o Numero!
-                </li>   
-            `
-        } else  {
-
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[3]}</strong>, è diverso dal 4o Numero!
-                </li>   
-            `
-        }
-
-        // arrayPrompt[4]
-        if (arrayPrompt[4] == array[4]) {
-            
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[4]}</strong>, è lo stesso del 5o Numero!
-                </li>   
-            `
-        } else  {
-
-            ulNumeri.innerHTML += 
-            `
-                <li>
-                    Il numero che hai inserito: 
-                    <strong>${arrayPrompt[4]}</strong>, è diverso dal 5o Numero!
-                </li>   
-            `
-        }
-    }
-
-    // Funzione per fermare SetInterval dopo 1 millisecondo
-    setTimeout( function() {
-
-        clearInterval(myInterval)
-    }, 1)
-
-    console.log(arrayPrompt)
-
 }
+
+// Funzione che parte dopo che la lista è sparita
+function myCountdown2() {     
+    
+    while ( arrayPrompt.length < 5 ) {
+
+        arrayPrompt.push( parseInt( prompt("Inserisci un numero") ) );
+    }
+}
+
+console.log(arrayPrompt)
